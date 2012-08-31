fe79445f7edefae3586f23f1ca1cc420c48ec8b9
Vitaly.Makhaev@gmail.com <Vitaly.Makhaev@gmail.com@1e12bd36-9ee4-eb99-c48b-55f1c2ccfa79>
2012-08-31 12:34:58 +0000

    com_help версия 6

--- components/com_hello/hello.php
+++ components/com_hello/hello.php
@@ -1,35 +1,32 @@
 <?php
 /**
+ * Hello World entry point file for Hello World Component
+ * 
  * @package    Joomla.Tutorials
  * @subpackage Components
- * components/com_hello/hello.php
- * @link http://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_1#Creating_the_Entry_Point
- * @license    GNU/GPL
+ * @link http://dev.joomla.org/component/option,com_jd-wiki/Itemid,31/id,tutorials:components/
+ * @license		GNU/GPL
  */
 
-// No direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+// no direct access
+defined('_JEXEC') or die('Restricted access');
 
 // Require the base controller
-
-require_once( JPATH_COMPONENT.DS.'controller.php' );
+require_once (JPATH_COMPONENT.DS.'controller.php');
 
 // Require specific controller if requested
-if ($controller = JRequest::getWord('controller')) {
-	$path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
-	if (file_exists($path)) {
-		require_once $path;
-	} else {
-		$controller = '';
-	}
+if($controller = JRequest::getVar('controller')) {
+	require_once (JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php');
 }
 
 // Create the controller
 $classname	= 'HelloController'.$controller;
-$controller	= new $classname();
+$controller = new $classname();
 
 // Perform the Request task
-$controller->execute( JRequest::getVar( 'task' ) );
+$controller->execute( JRequest::getVar('task'));
 
 // Redirect if set by the controller
 $controller->redirect();
+
+?>
