0ef5e15691ddbd3a21f7ba3fbcd3adc6ea6942f2
Vitaly.Makhaev@gmail.com <Vitaly.Makhaev@gmail.com@1e12bd36-9ee4-eb99-c48b-55f1c2ccfa79>
2012-08-31 16:45:10 +0000

    Телефоны подтягиваются из контактов, только отображаются.

--- trunk/components/com_user/views/user/tmpl/form.php
+++ trunk/components/com_user/views/user/tmpl/form.php
@@ -15,55 +15,51 @@ defined('_JEXEC') or die('Restricted access'); ?>
 		<?php echo $this->escape($this->params->get('page_title')); ?>
 	</div>
 <?php endif; ?>
+<?php $db = JFactory::getDBO();
+	$query = 'SELECT mobile FROM #__contact_details where user_id = '.$this->user->get('id');
+	$db->setQuery( $query );
+	$telephone1 = $db->loadResult();
+	$query = 'SELECT telephone FROM #__contact_details where user_id = '.$this->user->get('id');
+	$db->setQuery( $query );
+	$telephone2 = $db->loadResult();
+	$query = 'SELECT fax FROM #__contact_details where user_id = '.$this->user->get('id');
+	$db->setQuery( $query );
+	$telephone3 = $db->loadResult();
+?>
 <table cellpadding="5" cellspacing="0" border="0" width="100%">
 <tr>
-	<td>
-		<label for="username">
-			<?php echo JText::_( 'User Name' ); ?>:
-		</label>
+	<td width="20%"> <label for="username"> <?php echo JText::_( 'User Name' ); ?>: </label>
 	</td>
-	<td>
-		<span><?php echo $this->user->get('username');?></span>
+	<td width="20%"> <span><?php echo $this->user->get('username');?></span>
+	</td>
+	<td width="60%"> 
 	</td>
 </tr>
 <tr>
-	<td width="120">
-		<label for="name">
-			<?php echo JText::_( 'Your Name' ); ?>:
-		</label>
+	<td width="20%"> <label for="name"> <?php echo JText::_( 'Your Name' ); ?>: </label>
 	</td>
-	<td>
+	<td width="20%">
 		<input class="inputbox required" type="text" id="name" name="name" value="<?php echo $this->escape($this->user->get('name'));?>" size="40" />
 	</td>
 </tr>
 <tr>
-	<td width="120">
-		<label for="telephone1">
-			<?php echo JText::_( 'Telephone1' ); ?>:
-		</label>
+	<td width="20%"> <label for="telephone1"> <?php echo JText::_( 'Telephone1' ); ?>: </label>
 	</td>
-	<td>
-		<input class="inputbox" type="text" id="telephone1" name="telephone1" value="<?php echo $this->escape($this->user->get('telephone1'));?>" size="40" />
+	<td width="20%"><label for="telephone1"><?php echo $telephone1; ?></label>
+	</td>
+	<td width="60%"> <label for="telephone2wrong"> <?php echo JText::_( 'CONTACT US WHEN TELEPHONE WRONG' ); ?> </label>
 	</td>
 </tr>
 <tr>
-	<td width="120">
-		<label for="telephone2">
-			<?php echo JText::_( 'Telephone2' ); ?>:
-		</label>
+	<td width="20%"> <label for="telephone2"> <?php echo JText::_( 'Telephone2' ); ?>: </label>
 	</td>
-	<td>
-		<input class="inputbox" type="text" id="telephone2" name="telephone2" value="<?php echo $this->escape($this->user->get('telephone2'));?>" size="40" />
+	<td width="20%"> <label for="telephone2"> <?php echo $telephone2; ?> </label>
 	</td>
 </tr>
 <tr>
-	<td width="120">
-		<label for="telephone3">
-			<?php echo JText::_( 'telephone3' ); ?>:
-		</label>
+	<td width="20%"> <label for="telephone3"> <?php echo JText::_( 'telephone3' ); ?>: </label>
 	</td>
-	<td>
-		<input class="inputbox" type="text" id="telephone3" name="telephone3" value="<?php echo $this->escape($this->user->get('telephone3'));?>" size="40" />
+	<td width="20%"> <label for="telephone3"> <?php echo $telephone3; ?> </label>
 	</td>
 </tr>
 <tr>
